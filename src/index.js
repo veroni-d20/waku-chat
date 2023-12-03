86aca8abcbbdc5c05181d04d02a8f750202e44df
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-12-03 18:46:55 +0530

    waku chat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { LightNodeProvider } from "@waku/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Set the Light Node options
+const NODE_OPTIONS = { defaultBootstrap: true };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LightNodeProvider options={NODE_OPTIONS}>
+      <App />
+    </LightNodeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
